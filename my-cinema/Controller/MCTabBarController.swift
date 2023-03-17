72b38e7e948477660604ac021c9a59ea7a029b86
Thiago Leite <thiago.0686@gmail.com>
2023-03-17 17:58:29 -0300

    Add controllers

--- my-cinema/Controller/MCTabBarController.swift
+++ my-cinema/Controller/MCTabBarController.swift
@@ -11,8 +11,35 @@ class MCTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        setupTabs()
+    }
+    
+    private func setupTabs() {
+        let movieVC = MoviesViewController()
+        let seriesVC = SeriesViewController()
+        let favoritesVC = FavoritesViewController()
+        let profileVC = ProfileViewController()
+        
+        movieVC.title = "Fimes"
+        seriesVC.title = "Séries"
+        favoritesVC.title = "Favoritos"
+        profileVC.title = "Perfíl"
+        
+        let nav1 = UINavigationController(rootViewController: movieVC)
+        let nav2 = UINavigationController(rootViewController: seriesVC)
+        let nav3 = UINavigationController(rootViewController: favoritesVC)
+        let nav4 = UINavigationController(rootViewController: profileVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Filmes", image: UIImage(systemName: "popcorn"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Séries", image: UIImage(systemName: "play.display"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Favoritos", image: UIImage(systemName: "star"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Perfíl", image: UIImage(systemName: "person"), tag: 4)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
     }
 
 }
