e049b7a52398d7eb7fef4cd1faf6b08e382c5c19
Thiago Leite <thiago.0686@gmail.com>
2023-03-17 17:17:25 -0300

    feat: remove storyboards and add view programmatically

---
+++ my-cinema/Controller/MCTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MCTabBarController.swift
+//  my-cinema
+//
+//  Created by Thiago Leite on 17/03/23.
+//
+
+import UIKit
+
+class MCTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+}
